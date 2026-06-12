5d99aa337fea9b4d74ed9c3390cc9277c9a31e85
Liebe_Chaeso <d131215@naver.com>
2026-06-12 14:22:51 +0900

    REACT: 실습1: Counter 앱 만들기 레이아웃

--- lectures/onebite_react/section07/src/App.jsx
+++ lectures/onebite_react/section07/src/App.jsx
@@ -1,7 +1,26 @@
 import "./App.css";
+import { useState } from "react";
+import Viewer from "./components/Viewer";
+import Controller from "./components/Controller";
 
 function App() {
-  return <>카운터 앱</>;
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <h1>Simple Counter</h1>
+      <section>
+        <Viewer count={count} />
+      </section>
+      <section>
+        <Controller
+          controlNumber={() => {
+            setCount;
+          }}
+        />
+      </section>
+    </div>
+  );
 }
 
 export default App;
