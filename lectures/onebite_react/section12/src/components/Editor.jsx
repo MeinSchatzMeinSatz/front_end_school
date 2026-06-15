366fa16751d6c2671adb37b1a426acab2edf0b2d
Liebe_Chaeso <d131215@naver.com>
2026-06-15 18:27:51 +0900

    REACT: useContext를 사용해서 Context 생성

--- lectures/onebite_react/section12/src/components/Editor.jsx
+++ lectures/onebite_react/section12/src/components/Editor.jsx
@@ -1,7 +1,10 @@
 import "./Editor.css";
-import { useState, useRef } from "react";
+import { useState, useRef, useContext } from "react";
+import { TodoContext } from "../App";
 
-const Editor = ({ onCreate }) => {
+const Editor = () => {
+  const { onCreate } = useContext(TodoContext);
+
   const [content, setContent] = useState("");
   const contentRef = useRef();
 
@@ -13,15 +16,15 @@ const Editor = ({ onCreate }) => {
     if (e.keyCode === 13) {
       onSubmit();
     }
-  }; // 엔터키를 눌렀을 때 제출되도록 설정
+  };
 
   const onSubmit = () => {
     if (content === "") {
       contentRef.current.focus();
       return;
-    } // 빈 칸으로 제출했을 때 제출 안시키고, 다시 포커스 시키기
+    }
     onCreate(content);
-    setContent(""); // 새로운 투두 업데이트 후 비우기
+    setContent("");
   };
 
   return (
