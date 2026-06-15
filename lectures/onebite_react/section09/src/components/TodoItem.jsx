9681ccf41aaadd7f70fbbd70865a9d730162f39e
Liebe_Chaeso <d131215@naver.com>
2026-06-15 13:07:48 +0900

    REACT: 투두리스트 아이템 수정하기

--- lectures/onebite_react/section09/src/components/TodoItem.jsx
+++ lectures/onebite_react/section09/src/components/TodoItem.jsx
@@ -1,9 +1,18 @@
 import "./TodoItem.css";
 
-const TodoItem = ({ id, isDone, content, date }) => {
+const TodoItem = ({ id, isDone, content, date, onUpdate }) => {
+  const onChangeCheckbox = () => {
+    onUpdate(id);
+  };
+
   return (
     <div className="TodoItem">
-      <input type="checkbox" checked={isDone} readOnly />
+      <input
+        onChange={onChangeCheckbox}
+        type="checkbox"
+        checked={isDone}
+        readOnly
+      />
       <div className="content">{content}</div>
       <div className="date">{new Date(date).toLocaleDateString()}</div>
       <button>삭제</button>
