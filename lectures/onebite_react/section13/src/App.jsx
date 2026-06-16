376d84875d77390c9ce8c52c1b3367ce801538b3
Liebe_Chaeso <d131215@naver.com>
2026-06-16 13:45:33 +0900

    REACT: routing 하는 방법

--- lectures/onebite_react/section13/src/App.jsx
+++ lectures/onebite_react/section13/src/App.jsx
@@ -1,7 +1,27 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Diary from "./pages/Diary";
+import New from "./pages/New";
+import NotFound from "./pages/NotFound";
 
+// 1. "/" : 모든 일기를 조회하는 Home 페이지
+// 2. "/new" : 새로운 일기를 작성하는 New 페이지
+// 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        {/* 1. Routes 컴포넌트 안에는 Route 컴포넌트만 들어갈 수 있다 */}
+        {/* 2. Routes 컴포넌트 바깥에 있는 요소는 전부 렌더링 된다 */}
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary" element={<Diary />} />
+        {/* Not Found 처리 */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
--- lectures/onebite_react/section13/src/main.jsx
+++ lectures/onebite_react/section13/src/main.jsx
@@ -2,5 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  // 1. 브라우저 라우터는 브라우저의 현재 주소를 저장하고 감지하는 역할을 한다
+  // 컨텍스트를 통해 Navigation.Provider와 Location.Provider의 기능을 제공하고 있다.
+  // 즉, BrowserRouter를 통해 라우팅과 관련된 모든 데이터를 공급받을 수 있다.
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
