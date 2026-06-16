223e8871e85f4880f291dcfc5cc37246de55a5af
Liebe_Chaeso <d131215@naver.com>
2026-06-16 18:19:31 +0900

    REACT: 일기데이터 목업 데이터 설정, Edit 컴포넌트 제작

--- lectures/onebite_react/section13/src/App.jsx
+++ lectures/onebite_react/section13/src/App.jsx
@@ -1,49 +1,41 @@
 import "./App.css";
+import { useReducer } from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
+import Edit from "./pages/Edit";
 import NotFound from "./pages/NotFound";
-import Button from "./components/Button";
-import Header from "./components/Header";
 
-import { getEmotionImage } from "./util/getEmotionImage";
+const mockData = [
+  {
+    id: 1,
+    createdDate: new Date().getTime(),
+    emotionId: 1,
+    content: "1번 일기 내용",
+  },
+  {
+    id: 2,
+    createdDate: new Date().getTime(),
+    emotionId: 2,
+    content: "2번 일기 내용",
+  },
+];
+
+function reducer(state, action) {
+  return state;
+}
 
 function App() {
+  const [data, dispatch] = useReducer(reducer, mockData);
+
   return (
     <>
-      <Header
-        title={"Header"}
-        leftChild={<Button text={"Left"} />}
-        rightChild={<Button text={"Right"} />}
-      />
-      <Button
-        text={"123"}
-        type={"DEFAULT"}
-        onClick={() => {
-          console.log("123번 버튼 클릭");
-        }}
-      />
-
-      <Button
-        text={"123"}
-        type={"POSITIVE"}
-        onClick={() => {
-          console.log("123번 버튼 클릭");
-        }}
-      />
-
-      <Button
-        text={"123"}
-        type={"NEGATIVE"}
-        onClick={() => {
-          console.log("123번 버튼 클릭");
-        }}
-      />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
         <Route path="/diary/:id" element={<Diary />} />
+        <Route path="/edit/:id" element={<Edit />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
