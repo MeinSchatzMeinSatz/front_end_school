bfde71172c9afac9f4609837d734e768ac268cad
Liebe_Chaeso <d131215@naver.com>
2026-06-16 14:03:47 +0900

    REACT: Link, useNavigate를 활용한 페이지 이동

--- lectures/onebite_react/section13/src/App.jsx
+++ lectures/onebite_react/section13/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
@@ -9,8 +9,30 @@ import NotFound from "./pages/NotFound";
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 function App() {
+  const nav = useNavigate();
+
   return (
     <>
+      <div>
+        {/* 클라이언트 사이드 렌더링 */}
+        <Link to={"/"}>Home</Link>
+        <Link to={"/new"}>New</Link>
+        <Link to={"/diary"}>Diary</Link>
+
+        {/* 서버 사이드 렌더링 */}
+        <a href="/">Home</a>
+        <a href="/new">New</a>
+        <a href="/diary">Diary</a>
+      </div>
+
+      {/* 이벤트 핸들러에 페이지 이동 */}
+      <button
+        onClick={() => {
+          nav("/new");
+        }}
+      >
+        New 페이지로 이동
+      </button>
       <Routes>
         {/* 1. Routes 컴포넌트 안에는 Route 컴포넌트만 들어갈 수 있다 */}
         {/* 2. Routes 컴포넌트 바깥에 있는 요소는 전부 렌더링 된다 */}
