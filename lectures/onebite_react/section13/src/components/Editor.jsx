9ea5e55f873ae842148e5cce81391d87af6d2077
Liebe_Chaeso <d131215@naver.com>
2026-06-19 14:16:17 +0900

    REACT: Edit 페이지 레이아웃 작업 및 기능 구현

--- lectures/onebite_react/section13/src/components/Editor.jsx
+++ lectures/onebite_react/section13/src/components/Editor.jsx
@@ -1,7 +1,7 @@
 import Button from "./Button";
 import "./Editor.css";
 import EmotionItem from "./EmotionItem";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const emotionList = [
@@ -29,13 +29,22 @@ const getStringedDate = (targetDate) => {
   return `${year}-${month}-${date}`;
 };
 
-const Editor = ({ onSubmit }) => {
+const Editor = ({ initData, onSubmit }) => {
   const nav = useNavigate();
   const [input, setInput] = useState({
     createdDate: new Date(),
     emotionId: 3,
     content: "",
   });
+
+  useEffect(() => {
+    if (initData) {
+      setInput({
+        ...initData,
+        createdDate: new Date(Number(initData.createdDate)),
+      });
+    }
+  }, [initData]);
 
   const onChangeInput = (e) => {
     let name = e.target.name;
