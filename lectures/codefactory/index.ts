1562db60bd1b4c50906f176adb9d0e828e2c52f0
Liebe_Chaeso <d131215@naver.com>
2026-06-24 15:47:20 +0900

    260624: js의 문제점

---
+++ lectures/codefactory/index.ts
@@ -0,0 +1,8 @@
+function add(num1: number, num2: number) {
+  return num1 + num2;
+}
+
+console.log(add(1, 2));
+// console.log(add(1, "2")); // 코드 실행 전 단계에서 오류를 추려낼 수 있다.
+
+// 의도한 코드 그대로 상대방에게 코드를 전달할 수 있다.
