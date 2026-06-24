48a5df32659b72a8c7d72958cfdb49263be375b6
Liebe_Chaeso <d131215@naver.com>
2026-06-24 20:57:26 +0900

    260624: enum

--- lectures/onebite_ts/section2/dist/chapter4.js
+++ lectures/onebite_ts/section2/dist/chapter4.js
@@ -22,7 +22,20 @@ let user2 = {
     name: "Kim",
     nickName: "Kimchi",
     birth: "1995.05.03",
-    bio: "안녕하세요. 프론트엔드 개발자 이채준입니다.",
+    bio: "안녕하세요. 프론트엔드 개발자 김치찌개입니다.",
     location: "광진구",
 };
+let countryCodes = {
+    Korea: "ko",
+    UnitedStates: "us",
+    Japan: "jp",
+};
+let countryNumberCodes = {
+    Korea: 410,
+    States: 840,
+    jp: 123,
+};
+let countryNumberCodes2 = {
+    Korea: 410,
+}; // 아무런 프로퍼티가 없으면 규칙을 위반할 프로퍼티가 없어서 그냥 통과됨.
 export {};
