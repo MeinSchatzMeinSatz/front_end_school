32155db26ea58186963834ec502c098fcde88d21
Liebe_Chaeso <d131215@naver.com>
2026-07-01 19:27:57 +0900

    260701: 클래스 접근제어자

--- lectures/onebite_ts/section6/src/chapter2.ts
+++ lectures/onebite_ts/section6/src/chapter2.ts
@@ -1,4 +1,52 @@
 /**
  * 접근 제어자
  * access modifier
+ * => public private protected
+ * public: 아무 제약이 없는 상태
+ * private: 가장 제한적인 접근 제어자: 심지어 파생 클라스에서도 접근 불가
+ * protected: 외부에서는 접근 불가, 파생 클라스에선 접근 가능
  */
+
+class Employee {
+  // 필드
+  protected name: string;
+  age: number;
+  position: string;
+
+  constructor(name: string, age: number, position: string) {
+    this.name = name;
+    this.age = age;
+    this.position = position;
+  }
+
+  // 메서드
+  work() {
+    console.log(`${this.name} is working now`);
+  }
+}
+
+class ExecutiveOfficer extends Employee {
+  // 필드
+  officeNumber: number;
+
+  constructor(
+    name: string,
+    age: number,
+    position: string,
+    officeNumber: number
+  ) {
+    super(name, age, position);
+    this.officeNumber = officeNumber;
+  }
+
+  func() {
+    this.name;
+  }
+}
+
+const employee = new Employee("이채준", 31, "developer");
+// employee.name = "홍길동";
+// employee.age = 35;
+employee.position = "designer";
+
+console.log(employee);
