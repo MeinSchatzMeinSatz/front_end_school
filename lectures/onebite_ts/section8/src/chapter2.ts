eaa18199a07b3b9706e86fd777e6091f4e1c2873
Liebe_Chaeso <d131215@naver.com>
2026-07-02 09:07:27 +0900

    260702: keyof 연산자를 활용한 타입정의

--- lectures/onebite_ts/section8/src/chapter2.ts
+++ lectures/onebite_ts/section8/src/chapter2.ts
@@ -11,3 +11,31 @@ type PersonKey = keyof Person;
 // 즉, `keyof Person`을 실행해서 Person 인터페이스의 키 이름들을 뽑아서 문자열 리터럴 유니온 타입으로 만들어 준다.
 
 const personkey: PersonKey = "isOld";
+
+// person의 타입은 Person으로 쉽게 정해줄 수 있다.
+// 하지만 person의 key값에 대해서 타입을 정해줄땐 keyof 연산자를 사용하면 된다.
+function getPropertyKey(person: Person, key: PersonKey) {
+  return person[key];
+}
+
+const person: Person = {
+  name: "이채준",
+  age: 31,
+  isOld: false,
+};
+
+console.log(getPropertyKey(person, "name")); // 이채준
+
+// typeof 연산자와 적용
+
+const person2 = {
+  name: "이채준",
+  age: 31,
+  isOld: false,
+};
+
+type Person2 = typeof person2;
+
+function getPropertyKey2(person: Person, key: keyof typeof person) {
+  return person[key];
+}
