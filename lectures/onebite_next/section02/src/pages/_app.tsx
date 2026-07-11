b3f7f57f82ea9d3955fbf090b29d1f648697a497
Liebe_Chaeso <d131215@naver.com>
2026-07-11 15:15:11 +0900

    260711: next의 네비게이팅 기능

--- lectures/onebite_next/section02/src/pages/_app.tsx
+++ lectures/onebite_next/section02/src/pages/_app.tsx
@@ -1,9 +1,28 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  const onClickButton = () => {
+    router.push("/test");
+    /**
+     * 이 외에도 replace, back 같은 메서드도 있다.
+     */
+  };
+
   return (
     <>
+      <Link href={"/"}>index</Link>
+      &nbsp;
+      <Link href={"/search"}>search</Link>
+      &nbsp;
+      <Link href={"/book/1"}>book/1</Link>
+      <div>
+        <button onClick={onClickButton}>/test 페이지로 이동</button>
+      </div>
       <Component {...pageProps} />
     </>
   );
