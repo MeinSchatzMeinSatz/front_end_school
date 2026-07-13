cc9f292e3822a9f6c34ffa853e53e1ec5902fdd5
Liebe_Chaeso <d131215@naver.com>
2026-07-13 09:03:47 +0900

    260713: prefetching 개념 학습

--- lectures/onebite_next/section02/src/pages/_app.tsx
+++ lectures/onebite_next/section02/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -13,11 +14,17 @@ export default function App({ Component, pageProps }: AppProps) {
      */
   };
 
+  useEffect(() => {
+    router.prefetch("/test");
+  }, []);
+
   return (
     <>
       <Link href={"/"}>index</Link>
       &nbsp;
-      <Link href={"/search"}>search</Link>
+      <Link href={"/search"} prefetch={false}>
+        search
+      </Link>
       &nbsp;
       <Link href={"/book/1"}>book/1</Link>
       <div>
