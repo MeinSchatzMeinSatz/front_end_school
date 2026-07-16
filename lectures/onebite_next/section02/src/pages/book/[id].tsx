445987617e739ab73e99ccdf3a181007607daeb7
Liebe_Chaeso <d131215@naver.com>
2026-07-16 17:42:24 +0900

    260716: SSR 개념 학습 및 실습

--- lectures/onebite_next/section02/src/pages/book/[id].tsx
+++ lectures/onebite_next/section02/src/pages/book/[id].tsx
@@ -1,9 +1,28 @@
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
 import style from "./[id].module.css";
 import books from "@/mock/book.json";
+import fetchOneBook from "@/lib/fetch-one-book";
 
-export default function Page() {
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const id = context.params!.id;
+  const book = await fetchOneBook(Number(id));
+  return {
+    props: {
+      book,
+    },
+  };
+};
+
+export default function Page({
+  book,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  if (!book) {
+    return "문제가 발생했습니다. 다시 시도하세요.";
+  }
   const { id, title, subTitle, description, author, publisher, coverImgUrl } =
-    books[0];
+    book;
 
   return (
     <div className={style.container}>
--- lectures/onebite_next/section02/src/pages/search/index.tsx
+++ lectures/onebite_next/section02/src/pages/search/index.tsx
@@ -2,9 +2,23 @@ import SearchableLayout from "@/components/searchable-layout";
 import { useRouter } from "next/router";
 import { ReactNode } from "react";
 import BookItem from "@/components/BookItem";
-import books from "@/mock/book.json";
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import fetchBooks from "@/lib/fetch-books";
 
-export default function Page() {
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const q = context.query.q;
+  const books = await fetchBooks(q as string);
+
+  return {
+    props: { books },
+  };
+};
+
+export default function Page({
+  books,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div>
       {books.map((book) => (
