b9671d9e22014fb0c4b053c8ddb5aa7919aae488
Liebe_Chaeso <d131215@naver.com>
2026-07-20 16:08:23 +0900

    260720: Next에서 SEO 설정하기

--- lectures/onebite_next/section02/src/pages/index.tsx
+++ lectures/onebite_next/section02/src/pages/index.tsx
@@ -1,4 +1,3 @@
-// CSS Module
 import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
 import { ReactNode } from "react";
@@ -6,10 +5,9 @@ import BookItem from "@/components/BookItem";
 import { InferGetStaticPropsType } from "next";
 import fetchBooks from "@/lib/fetch-books";
 import fetchRandomBooks from "@/lib/fetch-random-books";
+import Head from "next/head";
 
 export const getStaticProps = async () => {
-  console.log("인덱스 페이지");
-
   const [allBooks, recoBooks] = await Promise.all([
     fetchBooks(),
     fetchRandomBooks(),
@@ -20,7 +18,6 @@ export const getStaticProps = async () => {
       allBooks,
       recoBooks,
     },
-    // revalidate: 3,
   };
 };
 
@@ -30,6 +27,15 @@ export default function Home({
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <>
+      <Head>
+        <title>한입북스</title>
+        <meta property="og:image" content="/thumbnail.png" />
+        <meta property="og:title" content="한입북스" />
+        <meta
+          property="og:description"
+          content="한입 북스에 등록된 도서들을 만나보세요"
+        />
+      </Head>
       <div className={style.container}>
         <section>
           <h3>지금 추천하는 도서</h3>
