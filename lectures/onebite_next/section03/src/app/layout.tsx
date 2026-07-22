0b7c7b7605a217a87a8ad97af2b127d9f3a1cbbc
Liebe_Chaeso <d131215@naver.com>
2026-07-22 15:27:47 +0900

    250722: 앱 라우터에서의 페칭 방법 실습

--- lectures/onebite_next/section03/src/app/layout.tsx
+++ lectures/onebite_next/section03/src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
             <Link href={"/"}>📚 ONEBITE BOOKS</Link>
           </header>
           <main>{children}</main>
-          <footer>제작 @winterlood</footer>
+          <footer>제작 @leechaejun</footer>
         </div>
       </body>
     </html>
