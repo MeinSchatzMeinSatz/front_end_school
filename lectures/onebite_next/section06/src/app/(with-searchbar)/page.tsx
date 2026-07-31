3e04f32513e0dcc7f8404794514b340095554b6b
Liebe_Chaeso <d131215@naver.com>
2026-07-31 16:26:40 +0900

    260731: 스켈레톤 UI 적용하기

--- lectures/onebite_next/section06/src/app/(with-searchbar)/page.tsx
+++ lectures/onebite_next/section06/src/app/(with-searchbar)/page.tsx
@@ -1,6 +1,7 @@
 import style from "./page.module.css";
 import AllBooks from "@/components/all-books";
 import RecoBooks from "@/components/reco-books";
+import BookListSkeleton from "@/components/skeleton/book-list-skeleton";
 import { Suspense } from "react";
 
 export const dynamic = "force-dynamic";
@@ -10,13 +11,13 @@ export default async function Home() {
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        <Suspense fallback={<div>도서를 불러오는 중입니다...</div>}>
+        <Suspense fallback={<BookListSkeleton count={3} />}>
           <RecoBooks />
         </Suspense>
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        <Suspense fallback={<div>모든 도서를 불러오는 중입니다...</div>}>
+        <Suspense fallback={<BookListSkeleton count={5} />}>
           <AllBooks />
         </Suspense>
       </section>
